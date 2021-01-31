4d1506ad27771a8cdeb7809970ec1a25e65603e8
devwardhan <devwardhanpathak@gmail.com>
2021-01-31 23:17:33 +0530

    Header and Landing Pag completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+    Nav, Navbar, NavbarToggler, Collapse, NavItem
+} from 'reactstrap';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import '../css/header.css';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar className="my-nav" expand="md">
+                    <div className="container-fluid">
+                        <div className="mr-auto header-logo">Devwardhan</div>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav className="ml-auto nav-link" navbar>
+                                <NavItem className="menu-item">About Me</NavItem>
+                                <NavItem className="menu-item">Projects</NavItem>
+                                <NavItem className="menu-item">Contact Me</NavItem>
+                            </Nav>
+                        </Collapse>
+                        <NavbarToggler onClick={this.toggleNav}><span className="nav-icon"><DehazeIcon /></span></NavbarToggler>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../css/landing.css';
+import Header from './Header';
+import WebDevimg from '../images/webdev.svg';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+
+
+const Landing = () => {
+    return (
+        <>
+            <Header />
+            <div className="container-fluid mt-5">
+                <div className="row">
+                    <div className="col-lg m-auto">
+                        <div className="text-div">
+                            <h2 className="text1">Namaste, I am</h2>
+                            <h1 className="text2">Devwardhan Pathak</h1>
+                            <h2 className="text3">Front-End Developer</h2>
+                            <h3 className="text4">JavaScript | React | CSS</h3>
+                        </div>
+                        <div className="social-icons">
+                            <ul className="footer-icons">
+                                <li className="footer-li">
+                                    <a href="https://github.com/devwardhanpathak" rel="noreferrer" target="_blank">
+                                        <GitHubIcon className="material-icon" />
+                                    </a>
+                                </li>
+                                <li className="footer-li">
+                                    <a href="https://www.linkedin.com/in/devwardhan-pathak-b680b1158/" rel="noreferrer" target="_blank">
+                                        <LinkedInIcon className="material-icon" />
+                                    </a>
+                                </li>
+                                <li className="footer-li">
+                                    <a href="https://twitter.com/DevwardhanP" rel="noreferrer" target="_blank">
+                                        <TwitterIcon className="material-icon" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg">
+                        <div className="landing-image-div">
+                            <img className="img-fluid" src={WebDevimg} alt="image" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Landing;
